5feaef5cce552531c686056a82de6d86050468e4
Joabutt <89288499+Joabutt@users.noreply.github.com>
2022-12-31 00:52:07 +0800

    update cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const PORT = 3020
-const cors = require(cors)
+const cors = require("cors")
 app.use(cors())
 app.listen( PORT, () => {
     console.log("listening on port 3020")
